7af12c9f1c3c4d1d57680f75482f63bffd77426b
Abhinav4040 <abhijangra98@gmail.com>
2023-08-31 23:14:18 +0530

    Added selenoid grid

--- wdio.conf.js
+++ wdio.conf.js
@@ -5,6 +5,9 @@ exports.config = {
     // ====================
     // WebdriverIO supports running e2e tests as well as unit and component tests.
     runner: 'local',
+    hostname: "localhost",
+    port: 4444,
+    path: "/wd/hub",
     
     //
     // ==================
@@ -45,7 +48,7 @@ exports.config = {
     // and 30 processes will get spawned. The property handles how many capabilities
     // from the same test should run tests.
     //
-    maxInstances: 10,
+    maxInstances: 1,
     //
     // If you have trouble getting all important capabilities together, check out the
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
